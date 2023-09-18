e8ceec8502567e6859bb59e24ead07ff01f9df53
Irene Gitau <irene.gitau@strathmore.edu>
2023-09-18 12:04:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+const currentUser = 'Irene Annah'; // index.js
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+module.exports = {
+  welcomeMessage,
+  excitedWelcomeMessage,
+};
+
+const I = currentUser.charAt(0);
+const shortGreeting = "Welcome, ${I}!" ;
+
+module.exports = {
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting,
+};
+
